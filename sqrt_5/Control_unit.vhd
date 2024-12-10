063d7819b7f0eb9f821671e26b8b9e493cdb7240
Em1leprl <eprele@gmail.com>
2024-12-10 10:40:30 +0100

    Update Control_unit.vhd

--- sqrt_5/Control_unit.vhd
+++ sqrt_5/Control_unit.vhd
@@ -6,16 +6,12 @@ entity ControlUnit is
     generic(
         N: integer := 32 );
     Port (
-        A          : in  std_logic_vector(2*N-1 downto 0);
         start      : in  std_logic;
         clk        : in  std_logic;
         reset      : in  std_logic;
         done       : out std_logic;
-        result     : out std_logic_vector(N-1 downto 0);
-        sel_addsub : out std_logic;
-        in1_addsub : out std_logic_vector(N+1 downto 0);
-        in2_addsub : out std_logic_vector(N+1 downto 0);
-        out_addsub : in  std_logic_vector(N+1 downto 0)
+        sel_D_mux : out std_logic;
+        sel_result_mux : out std_logic
     );
 end ControlUnit;
 
@@ -23,10 +19,6 @@ architecture Behavioral of ControlUnit is
     type t_state is (sleep, init, compute, finished);
     signal state : t_state;
     signal counter : integer range 2*N-1 downto -1 := 0; 
-    signal D : std_logic_vector(2*N-1 downto 0);
-    signal R : std_logic_vector(N-1 downto 0); -- Inclut 1 bit supplémentaire pour le MSB
-    signal Q : std_logic_vector(N-1 downto 0);
-    signal R_MSB : std_logic;
 begin
     -- Process principal
     process(clk, reset)
@@ -35,55 +27,39 @@ begin
             state <= sleep; 
             done <= '0';
             counter <= N;
-            D <= (others => '0');
-            R <= (others => '0');
-            Q <= (others => '0');
-            in1_addsub <= (others => '0');
-            in2_addsub <= (others => '0');
+            sel_D_mux <= '0';
+            sel_result_mux <= '0';
+            
         elsif rising_edge(clk) then
             case state is
                 when sleep =>
+                    sel_D_mux <= '0';
+                    sel_result_mux <= '0';
                     if start = '1' then
                         state <= init;
                     end if;
 
                 when init =>
                     -- Initialisation
-                    D <= A;
-                    R <= (others => '0');
-                    Q <= (others => '0');
-                    counter <= N;
+                    sel_D_mux <= '0';
+                    sel_result_mux <= '0';
                     done <= '0';
-                    state <= compute;
-                    R_MSB <= '0';
-                    in1_addsub <= (N+1 downto 2 => '0') & A(2*N-1 downto 2*N-2);
-                    in2_addsub <= (N+1 downto 1 => '0') & '1';
-                    result <= (others => '0');
+					counter <= N-1;
+					state <= compute;
 
                 when compute => 
                     if counter < 0 then 
                         state <= finished;
                     else
-                        -- Préparer les entrées pour le Add/Sub
-                        in1_addsub <= R & D(2*N-1 downto 2*N-2); -- Bits les plus significatifs de D
-                        in2_addsub <= Q & R_MSB & '1';           -- Concaténer Q, MSB et "1"
-                        sel_addsub <= R_MSB;                    -- Sélectionne addition ou soustraction
-                        
-                        -- Mettre à jour R, Q, et D
-                        R_MSB <= out_addsub(N+1);               -- MSB du résultat
-                        R <= out_addsub(N-1 downto 0);            -- Nouveau reste
-                        Q <= Q(N-2 downto 0) & not out_addsub(N+1); -- Mettre à jour Q avec le nouveau bit
-                        D <= std_logic_vector(shift_left(unsigned(D), 2)); -- Décaler D de 2 bits
-                        
-                        -- Décrémenter le compteur
-                        counter <= counter - 1;
-                        state <= compute;
-                        result <= Q;
+                        sel_D_mux <= '1';
+                        sel_result_mux <= '1';
+                        counter <= counter-1;
                     end if;
 
                 when finished =>
-                    result <= Q; -- Résultat final
-                    done <= '1'; -- Indiquer que le calcul est terminé
+                    sel_D_mux <= '0';
+                    sel_result_mux <= '1';
+                    done <= '1'; -- Indiquer que le calcul est terminÃ©
                     if start = '0' then
                         state <= sleep;
                         done <= '0';
