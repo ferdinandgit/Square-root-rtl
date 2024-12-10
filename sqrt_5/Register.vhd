95e994eb210fbc45c4ede88f6de230f960c077de
Ferdix <80453736+ferdinandgit@users.noreply.github.com>
2024-12-10 11:07:23 +0100

    Update Register.vhd
    correct the register code to have good behavior on data and data_out

--- sqrt_5/Register.vhd
+++ sqrt_5/Register.vhd
@@ -15,15 +15,19 @@ end Reg;
 
 architecture Behavioral of Reg is
     signal data : std_logic_vector(N_data-1 downto 0);
+    
 begin
     process(reset, clk)
+        variable temp: std_logic_vector(N_data-1 downto 0);
     begin
         if reset = '0' then
             data <= (others => '0'); -- Reset the data to all zeros
+            data_out <= (others => '0');
         elsif rising_edge(clk) then
+            temp := data;
             data <= data_in; -- Load input data on clock edge
+            data_out <= temp; 
         end if;
     end process;
-    data_out <= data; -- Assign the signal to output
 end Behavioral;
 
